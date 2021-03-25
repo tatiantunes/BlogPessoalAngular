63c05de58007fc657d9d7c9b7f44685af015c565
tatiantunes <tativive@hotmail.com>
2021-03-24 22:32:50 -0300

    feat(addComponent): add Tema

--- blogPessoal/src/app/app.module.ts
+++ blogPessoal/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { FormsModule } from '@angular/forms';
 import { InicioComponent } from './inicio/inicio.component';
 import {HashLocationStrategy, LocationStrategy} from '@angular/common';
+import { TemaComponent } from './tema/tema.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import {HashLocationStrategy, LocationStrategy} from '@angular/common';
     RodapeComponent,
     EntrarComponent,
     CadastrarComponent,
-    InicioComponent
+    InicioComponent,
+    TemaComponent
   ],
   imports: [
     BrowserModule,
---
+++ blogPessoal/src/app/tema/tema.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment.prod';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-tema',
+  templateUrl: './tema.component.html',
+  styleUrls: ['./tema.component.css']
+})
+export class TemaComponent implements OnInit {
+
+  constructor(
+    private router:Router
+  ) { }
+
+  ngOnInit() {
+
+    if(environment.token == '')
+    {
+      alert('Sua sessão expirou faça login novamente')
+      this.router.navigate(['/entrar'])
+    }
+  }
+
+}
